10f262675124305ab2f5e29d45b47144f3e826ab
Melvillian <melvillevt@gmail.com>
2024-03-07 08:30:02 -0500

    add coinbase-pro-python poetry dep

--- coinbase-dca/main.py
+++ coinbase-dca/main.py
@@ -1,5 +1,20 @@
 from .lib.replace_me import get, post, patch
 
+from coinbase.rest import RESTClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+
+api_key = "organizations/{org_id}/apiKeys/{key_id}"
+api_secret = (
+    "-----BEGIN EC PRIVATE KEY-----\nYOUR PRIVATE KEY\n-----END EC PRIVATE KEY-----\n"
+)
+
+client = RESTClient(api_key=api_key, api_secret=api_secret)
+
 
 def main():
     print("Hello, world!")
